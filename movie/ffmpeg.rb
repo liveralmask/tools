e8849be59a7887b84cf17c231d11eab33106a65f
liveralmask <liveralmask.lisk@gmail.com>
2017-03-29 01:21:15 +0900

    refactoring

--- movie/ffmpeg.rb
+++ movie/ffmpeg.rb
@@ -1,4 +1,7 @@
-require "../common"
+#!/usr/bin/env ruby
+
+require "ult"
+extend Ult
 
 def ffmpeg( command )
   puts command
@@ -9,8 +12,8 @@ def ffmpeg( command )
   end
 end
 
-type = ARGV.shift
-case type
+action = ARGV.shift
+case action
 when "join"
   input_files = []
   output_file = "output.mp4"
@@ -40,9 +43,16 @@ when "join"
   
   # mp4 => ts
   ts_files = []
+  tmp_files = []
   input_files.each_with_index{|input_file, i|
-    ts_file = "#{i}.ts"
-    ffmpeg( "ffmpeg -i #{input_file} -c copy -f mpegts -y #{ts_file}" )
+    case Ult.extname( input_file )
+    when "ts"
+      ts_file = input_file
+    else
+      ts_file = ".tmp_#{i}.ts"
+      tmp_files.push ts_file
+      ffmpeg( "ffmpeg -i #{input_file} -c copy -f mpegts -y #{ts_file}" )
+    end
     ts_files.push "file '#{ts_file}'"
   }
   
@@ -54,13 +64,13 @@ when "join"
   ffmpeg( "ffmpeg -f concat -i #{concat_file} -vf scale=#{scale} -y #{output_file}" )
   
   # delete temporary files
-  input_files.each_with_index{|input_file, i|
-    rmfile( "#{i}.ts" )
+  tmp_files.each_with_index{|tmp_file|
+    rmfile( "#{tmp_file}" )
   }
   rmfile( concat_file )
   
   puts "Joined: #{output_file}"
 else
-  puts "Unknown type: #{type}"
+  puts "Unknown action: #{action}"
   exit 1
 end
